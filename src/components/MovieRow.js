c45d8d05d5fe0c6bcd89b25dfca3d300e82fd991
José Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-09-07 17:54:05 -0300

    start styling featured movie

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './MovieRow.css';
 
 export default ({ title, items }) => (
-  <div>
+  <div className="movieRow">
     <h2>{title}</h2>
     <div className="movieRow--listarea">
       <div className="movieRow--list">
-        {items.results.length > 0 && items.results.map((item, key) => (
-          <div className="movieRow--item" key={key}>
-            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
-          </div>
-        ))}
+        {items.results.length > 0 &&
+          items.results.map((item, key) => (
+            <div className="movieRow--item" key={key}>
+              <img
+                src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                alt={item.original_title}
+              />
+            </div>
+          ))}
       </div>
     </div>
   </div>
