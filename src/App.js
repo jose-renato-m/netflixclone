6fc2a653a2cc23592844a6a928408249e7c835bb
José Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-09-12 19:22:14 -0300

    start styling header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
@@ -30,6 +31,9 @@ export default () => {
 
   return (
     <div className="page">
+
+      <Header />
+
       {featuredData && <FeaturedMovie item={featuredData} />}
 
       <section className="lists">
